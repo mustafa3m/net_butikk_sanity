669cd27d8c006c5518cebe8eb8b87b5b1ef835ef
mustafa muse <muse3mm@gmail.com>
2022-06-01 19:45:07 +0200

    delete i header.

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -4,7 +4,7 @@
 
     <ul>
 
-      <li ><img class="logo-image" src="images/tea-logo.webp" alt="image-logo" width="50px"></li>
+      <li><img class="logo-image" src="images/tea-logo.webp" alt="image-logo" width="50px"></li>
       <li><router-link :to="{ name: 'home' }">HOME</router-link></li>
      
     </ul>
@@ -15,13 +15,10 @@
 
 <style scope>
   header {
-    
-    background-image: url(../img/bg.jpg);
-    background-repeat: no-repeat;
-    background-position: center bottom;
-    background-color: #96b011;
+   
+    background-color: var(--background-color);
     padding: 1rem;
-    color: white;
+    color: var(white);
     width: 100%;
     
   }
@@ -43,7 +40,7 @@
     margin: 0 auto;
     height: auto;
   }
-  @media screen and (max-width: 550px) {
+  @media screen and (max-width: 600px) {
  
     header{
       padding: 0;
