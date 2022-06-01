7020d61f43eb8fd51b8984d49461321114ceccd4
mustafa muse <muse3mm@gmail.com>
2022-06-01 19:45:38 +0200

    Delete fil in store.

--- app/src/store.js
+++ app/src/store.js
@@ -1,16 +1,16 @@
 export default {
   state() {
     return {
-      results: [],
+      result: [],
       cart: [],
       product: [],
       quantity: 1,
       totalPrice: 0,
     };
   },
-  getters: {},
+  
   mutations: {
-    //addProductToCart(product)
+    //Legge producter i cart
     ADD_PRODUCT_TO_CART(state, product) {
       let existing = state.cart.findIndex((item) => {
         return item.product._id === product._id;
@@ -22,31 +22,22 @@ export default {
       } else {
         return state.cart.push({ quantity: 1, product: product }); //push product i cart
       }
-      //console.log(state.cart);
     },
+    // Slett produkter
     DELETE_PRODUCT(state, product) {
       state.cart.splice(product, 1);
     },
-    //change number of units for  an item
-    CHANGE_NUMBER_OF_UNITS(state, product) {
-      //console.log("quantity i sanity ", product.quantity);
-      let index = state.cart.findIndex(
-        (item) => item.product._id === product._id
-      );
 
-      state.cart[index].quantity =
-        state.cart[index].quantity + product.quantity;
-    },
-    // calculate subtotal SubTotalItems  ()  {
+    // Regn ut den totale summen av produktene
     SUB_TOTAL_ITEMS(state) {
       let totalPrice = state.cart.reduce((acc, cartItem) => {
-        //console.log("single årice", cartItem.product.price);
+      
         return (
           parseInt(acc) +
           parseInt(cartItem.product.price) * parseInt(cartItem.quantity)
         );
       }, 0);
-      //console.log(totalPrice);
+
       state.totalPrice = totalPrice;
     },
   },
