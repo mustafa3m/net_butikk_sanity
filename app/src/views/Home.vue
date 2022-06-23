9facfdd192479801485bf87def7e3a75aa67a972
mustafa muse <muse3mm@gmail.com>
2022-06-23 22:29:22 +0200

    Change  BEM css.

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -4,19 +4,19 @@
   <main class="container">
     <div v-if="loading">...</div>
 
-    <div id="tea" v-else>
-      <div class="listing-product" v-for="item in result" :key="item._id">
-        <h2 class="listing-product-title">{{ item.title }}</h2>
+    <div id="card" v-else>
+      <div class="card__product" v-for="item in result" :key="item._id">
+        <h2 class="card__title">{{ item.title }}</h2>
 
         <img
-          class="listing-product-image"
+          class="card__image"
           :src="item.image.asset.url"
           alt="image"
         />
 
         <h3>{{ item.name }}</h3>
 
-        <p class="listing-product-text">{{ item.description }}</p>
+        <p class="card__description">{{ item.description }}</p>
 
         <p>{{ item.price }} €</p>
 
@@ -68,10 +68,7 @@
   };
 </script>
 <style>
-  #tea {
-    display: flex;
-    margin-top: 2rem;
-  }
+ 
 
   .container {
     display: flex;
@@ -81,33 +78,36 @@
     position: relative;
   }
 
-  .listing-product-image {
+  /* Card  */
+
+   #card {
+    display: flex;
+    margin-top: 2rem;
+  }
+
+  .card__image {
     width: 50%;
     margin: 0 auto;
     height: auto;
   }
 
-  .listing-product-text {
+  .card__description {
     font-size: 1.5rem;
   }
 
-  main h1 {
-    font-family: var(--font-family);
-    font-weight: normal;
-    font-size: 4rem;
-  }
-  main h2 {
+
+  .card__title {
     font-family: var(--font-family);
     font-weight: normal;
     font-size: 2rem;
   }
 
-  main h2 span {
+  .card__title span {
     background-color: var(--color-white);
     padding: 10px;
   }
 
-  main h3 {
+  .card__product h3 {
     font-family: var(--font-family);
     font-weight: normal;
     font-size: 1.5rem;
@@ -115,7 +115,7 @@
     margin: 0;
   }
 
-  .btn {
+  .card__btn {
     background-color: var(--background-color-btn);
     color: var(--color-white);
     text-transform: uppercase;
@@ -128,49 +128,49 @@
     text-align: center;
   }
 
-  .btn:hover {
+  .card__btn:hover {
     background-color: var(--background-color-hover);
     color: var(--color-white);
   }
 
   @media screen and (max-width: 600px) {
-    #tea {
+     .container {
+      width: 100vw;
+    }
+
+    #card {
       flex-direction: column;
       margin: 1rem;
       width: 100%;
     }
-    .listing-product {
+    .card__product {
       margin-top: 2rem;
       width: 100%;
     }
 
-    .listing-product h2 {
+    .card__title {
       font-size: 1.3rem;
       margin-bottom: 0.7rem;
     }
-    .listing-product-image {
+    .card__image {
       margin-bottom: 1rem;
       width: 100%;
       height: auto;
     }
-    .listing-product-text {
+    .card__text {
       font-size: 1rem;
       width: 100%;
     }
-    .listing-product-title {
-      font-size: 1rem;
-    }
-    .btn {
+   
+    .card__btn {
       margin-bottom: 2rem;
     }
 
-    main h3 {
+    .card__product h3 {
       font-size: 1.5rem;
     }
 
-    .container {
-      width: 100vw;
-    }
+   
 
     
   }
